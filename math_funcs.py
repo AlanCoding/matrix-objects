ae0a6b76b9fc608f4f1e3be1dca6c7a42cef0a58
Alan R <alan.rominger@gmail.com>
2015-05-13 16:15:04 -0400

    Math functions work, tests written and passed

---
+++ math_funcs.py
@@ -0,0 +1,277 @@
+import math
+
+
+class ShapeException(Exception):
+    pass
+
+
+class TypeException(Exception):
+    pass
+
+
+class MatrixVector():
+
+    def __init__(self, some_list):
+        self.vals = some_list
+
+
+    def is_matrix(self):
+        v = self.vals
+        if type(v[0]) is list:
+            return True
+        return False
+
+    def shape(self):
+        u = self.vals
+        if self.is_matrix():
+            return (len(u), len(u[0]))
+        else:
+            return (len(u), )
+
+
+class Matrix(MatrixVector):
+
+    def __init__(self, A):
+        super().__init__(A)
+        if self.vals[0] is int:
+            for i in range(len(self.vals)):
+                self.vals[i] = [self.vals[i]]
+
+    def __str__(self):
+        rtrn = ""
+        for i in range(len(self.vals)):
+            for j in range(len(self.vals[0])):
+                rtrn += str(self.vals[i][j])+" "
+            rtrn += " \n"
+        return rtrn
+
+
+    def __eq__(self, other):
+        if type(other) is not Matrix:
+            return False
+        A = self.vals
+        B = other.vals
+        r, c = self.shape()
+        for i in range(r):
+            for j in range(c):
+                if A[i][j] != B[i][j]:
+                    return False
+        return True
+
+
+    def row(self, i):
+        A = self.vals
+        return Vector([j for j in A[i]])
+
+
+    def col(self, j):
+        A = self.vals
+        return Vector([A[i][j] for i in range(len(A))])
+
+
+    def matrix_scalar_multiply(self, a):
+        C = self.vals
+        return Matrix([[C[i][j]*a for j in range(len(C[0]))]
+                for i in range(len(C))])
+
+
+    def matrix_matrix_multiply(self, other):
+        A = self.vals
+        B = other.vals
+        if len(A[0]) != len(B):
+            raise ShapeException()
+        return Matrix([[sum(A[i][k]*B[k][j]
+                for k in range(len(A[0])))
+                for j in range(len(B[0]))]
+                for i in range(len(A))])
+
+
+    def matrix_vector_multiply(self, other):
+        A = self.vals
+        v = other.vals
+        vMat = Matrix([[val] for val in v])
+        matOut = self.matrix_matrix_multiply(vMat)
+        C = matOut.vals
+        return Vector([C[i][0] for i in range(len(C))])
+
+
+    def __add__(self, other):
+        if type(other) is not Matrix:
+            raise TypeException()
+        elif self.shape() != other.shape():
+            raise ShapeException()
+        A = self.vals
+        B = other.vals
+        r, c = self.shape()
+        return Matrix([[A[i][j]+B[i][j] for i in range(r)]
+                        for j in range(c)])
+
+
+    def __sub__(self, other):
+        A = self.vals
+        nB = other.matrix_scalar_multiply(-1)
+        return self.__add__(nB)
+
+
+    def __rmul__(self, other):
+        if type(other) is int or type(other) is float:
+            return self.matrix_scalar_multiply(other)
+        elif type(other) is Vector or type(other) is Matrix:
+            return other.__mul__(self)
+        else:
+            raise TypeException()
+
+
+    def __mul__(self,other):
+        if type(other) is int or type(other) is float:
+            return self.matrix_scalar_multiply(other)
+        elif type(other) is Vector:
+            return self.matrix_vector_multiply(other)
+        elif type(other) is Matrix:
+            return self.matrix_matrix_multiply(other)
+        else:
+            raise TypeException()
+
+
+class Vector(MatrixVector):
+
+
+    def __str__(self):
+        rtrn = ""
+        for i in range(len(self.vals)):
+            rtrn += " " + str(self.vals[i])
+        return rtrn
+
+    def __add__(self, other):
+        u = self.vals
+        w = other.vals
+        if self.shape() != other.shape():
+            raise ShapeException()
+        return Vector([i+j for i, j in zip(u, w)])
+
+
+    def vector_scalar_multiply(self, other):
+        return Vector([self.vals[i]*other
+                       for i in range(len(self.vals))])
+
+    def __sub__(self, other):
+        return self.__add__(other.vector_scalar_multiply(-1))
+
+
+    # def sum(self,*vectors):
+    #     u = self.vals
+    #     if self.is_inconsistent(*vectors):
+    #         raise ShapeException()
+    #     return [u[i]+sum(v[i] for v in vectors)
+    #             for i in range(len(vectors[0]))]
+
+
+    def dot(self, other):
+        w = self.vals
+        y = other.vals
+        if self.shape() != other.shape():
+            raise ShapeException()
+        return sum(i*j for i, j in zip(w, y))
+
+
+    def vector_matrix_multiply(self, other):
+        pass
+
+    def vector_vector_multiply(self, other):
+        v = self.vals
+        w = other.vals
+        return Vector([i*j for i,j in zip(v,w)])
+
+
+
+    def __mul__(self, other):
+        if type(other) is Vector:
+            return self.vector_vector_multiply(other)
+        elif type(other) is Matrix:
+            return self.vector_matrix_multiply(other)
+        elif type(other) is int or type(other) is float:
+            return self.vector_scalar_multiply(other)
+        else:
+            raise TypeException()
+
+    def __rmul__(self,other):
+        if type(other) is Matrix:
+            return other.matrix_vector_multiply(self)
+        elif type(other) is Vector:
+            return self.vector_vector_multiply(other)
+        elif type(other) is int or type(other) is float:
+            return self.vector_scalar_multiply(other)
+        else:
+            raise TypeException()
+
+    # def vector_mean(*vectors):
+    #     theSum = vector_sum(*vectors)
+    #     return [i/len(vectors) for i in theSum]
+
+    def magnitude(self):
+        v = self.vals
+        squares = sum(i**2 for i in v)
+        return squares**(0.5)
+
+    def __eq__(self, other):
+        if type(other) is not Vector:
+            return False
+        v = self.vals
+        u = other.vals
+        return sum(i==j for i,j in zip(v,u))
+
+
+if __name__ == '__main__':
+
+    m = [3, 4]
+    n = [5, 0]
+
+    v = [1, 3, 0]
+    w = [0, 2, 4]
+    u = [1, 1, 1]
+    y = [10, 20, 30]
+    z = [0, 0, 0]
+
+
+    A = [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]]
+    B = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+    C = [[1, 2],
+         [2, 1],
+         [1, 2]]
+    D = [[1, 2, 3],
+         [3, 2, 1]]
+
+    print(Vector(n))
+    print(Matrix(A).__str__())
+    print("hello world")
+    print(Vector(n)+Vector(m))
+    print(Vector(n)-Vector(m))
+    print(Vector(n)*Vector(m))
+    print(Vector(v)*Matrix(A))
+    print(' ')
+    print(3*Matrix(A))
+    print(Matrix(A)*3)
+    print(Matrix(A)*5.2)
+    print(" ---- class tests -----")
+    print(Vector([1, 2]) + Vector([0, 4]))
+    print(Vector([1, 2]) - Vector([0, 4]))
+    print(Vector([1, 2]) * 3)
+    print(" ---- equality -----")
+    print(Vector(y)==Vector(z))
+    print(Matrix(A)==Matrix(B))
+    print(Matrix(A)==Matrix(A))
+
+    print(Vector([1, 2]) == Vector([1, 2])) # results in True
+
+    print(Matrix([[0, 1], [1, 0]]) + Matrix([[1, 1], [0, 0]]))
+    print(Matrix([[0, 1], [1, 0]]) - Matrix([[1, 1], [0, 0]]))
+    print(Matrix([[0, 1], [1, 0]]) * 3)
+    print(" mat-vect multiply ")
+    print(Matrix([[0, 1], [1, 0]]) * Vector([1, 2]))
+#    print(Matrix([[1, 1, 1], [0, 0, 0]]) * Matrix([[1, 1], [2, 2], [3, 3]]))
+
+    print(Matrix([[0, 1], [1, 0]]) == Matrix([[1, 1], [0, 0]])) # results in False
---
+++ test_math.py
@@ -0,0 +1,213 @@
+from math_funcs import *
+from nose.tools import raises
+
+
+def is_equal(x, y, tolerance=0.001):
+    """Helper function to compare floats, which are often not quite equal
+    even when they should be."""
+    return abs(x - y) <= tolerance
+
+
+m = Vector([3, 4])
+n = Vector([5, 0])
+
+v = Vector([1, 3, 0])
+w = Vector([0, 2, 4])
+u = Vector([1, 1, 1])
+y = Vector([10, 20, 30])
+z = Vector([0, 0, 0])
+
+
+def test_shape_vectors():
+    """shape should take a vector or matrix and return a tuple with the
+    number of rows (for a vector) or the number of rows and columns
+    (for a matrix.)"""
+    assert m.shape() == (2,)
+    assert v.shape() == (3,)
+    assert Vector([1]).shape() == (1,)
+
+
+def test_vector_add():
+    """
+    [a b]  + [c d]  = [a+c b+d]
+
+    Matrix + Matrix = Matrix
+    """
+    assert v+w == Vector([1,5,4])
+    assert u+y == Vector([11,21,31])
+    assert u+z == u
+
+
+def test_vector_add_is_communicative():
+    assert w+y == y+w
+
+
+@raises(ShapeException)
+def test_vector_add_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    m+v
+
+
+def test_vector_sub():
+    """
+    [a b]  - [c d]  = [a-c b-d]
+
+    Matrix + Matrix = Matrix
+    """
+    assert v-w == Vector([1,1,-4])
+    assert w-v == Vector([-1,-1,4])
+    assert y-z == y
+
+
+@raises(ShapeException)
+def test_vector_sub_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    m-v
+
+
+def test_vector_sum():
+    """vector_sum can take any number of vectors and add them together."""
+    assert v+w+u+y+z == Vector([12,26,35])
+
+
+@raises(ShapeException)
+def test_vector_sum_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    v+w+m+y
+
+
+def test_dot():
+    """
+    dot([a b], [c d])   = a * c + b * d
+
+    dot(Vector, Vector) = Scalar
+    """
+    w.dot(y) == 160
+    m.dot(n) == 15
+    u.dot(z) == 0
+
+
+@raises(ShapeException)
+def test_dot_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    v.dot(m)
+
+
+def test_vector_multiply():
+    """
+    [a b]  *  Z     = [a*Z b*Z]
+
+    Vector * Scalar = Vector
+    """
+    assert v*0.5 == Vector([0.5,1.5,0])
+    assert m*2 == Vector([6,8])
+
+
+def test_magnitude():
+    """
+    magnitude([a b])  = sqrt(a^2 + b^2)
+
+    magnitude(Vector) = Scalar
+    """
+    assert m.magnitude() ==5
+    assert y.magnitude() == math.sqrt(1400)
+
+
+
+A = Matrix([[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]])
+B = Matrix([[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]])
+C = Matrix([[1, 2],
+     [2, 1],
+     [1, 2]])
+D = Matrix([[1, 2, 3],
+     [3, 2, 1]])
+
+
+def test_shape_matrices():
+    """shape should take a vector or matrix and return a tuple with the
+    number of rows (for a vector) or the number of rows and columns
+    (for a matrix.)"""
+    assert A.shape() == (3,3)
+    assert C.shape() == (3,2)
+    assert D.shape() == (2,3)
+
+
+def test_matrix_row():
+    """
+           0 1  <- rows
+       0 [[a b]]
+       1 [[c d]]
+       ^
+     columns
+    """
+    assert A.row(0) == Vector([1,0,0])
+    assert B.row(1) == Vector([4,5,6])
+    assert C.row(2) == Vector([1,2])
+
+
+def test_matrix_col():
+    """
+           0 1  <- rows
+       0 [[a b]]
+       1 [[c d]]
+       ^
+     columns
+    """
+    assert A.col(0) == Vector([1,0,0])
+    assert B.col(1) == Vector([2,5,8])
+    assert D.col(2) == Vector([3,1])
+
+
+def test_matrix_scalar_multiply():
+    """
+    [[a b]   *  Z   =   [[a*Z b*Z]
+     [c d]]              [c*Z d*Z]]
+
+    Matrix * Scalar = Matrix
+    """
+    assert C*3 == Matrix([[3,6],
+                            [6,3],
+                            [3,6]])
+
+
+def test_matrix_vector_multiply():
+    """
+    [[a b]   *  [x   =   [a*x+b*y
+     [c d]       y]       c*x+d*y
+     [e f]                e*x+f*y]
+
+    Matrix * Vector = Vector
+    """
+    assert A*Vector([2,5,4]) == Vector([2,5,4])
+
+
+@raises(ShapeException)
+def test_matrix_vector_multiply_checks_shapes():
+    """Shape Rule: The number of rows of the vector must equal the number of
+    columns of the matrix."""
+    C*Vector([1,2,3])
+
+
+def test_matrix_matrix_multiply():
+    """
+    [[a b]   *  [[w x]   =   [[a*w+b*y a*x+b*z]
+     [c d]       [y z]]       [c*w+d*y c*x+d*z]
+     [e f]                    [e*w+f*y e*x+f*z]]
+
+    Matrix * Matrix = Matrix
+    """
+    assert A*B == B
+    assert B*C == Matrix([[8, 10],
+                        [20, 25],
+                        [32, 40]])
+
+
+@raises(ShapeException)
+def test_matrix_matrix_multiply_checks_shapes():
+    """Shape Rule: The number of columns of the first matrix must equal the
+    number of rows of the second matrix."""
+    A*D
